5bfa7c894adf27f583e45a13c6a26e67badc23e3
sanu_kumar <sanukumar1138@gmail.com>
2021-06-23 00:45:15 +0530

    codemirror implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Editor from './Editor';
 
-function App() {
+const App = () => {
+  const [html, setHtml] = useState('');
+  const [srcDoc, setSrcDoc] = useState('');
+  const [theme, setTheme] = useState('');
+
+  const handleCodeRun = () => {
+    setSrcDoc(
+      `<html>
+      <body>${html}</body>
+    </html>`
+    );
+  };
+  const handleTheme = () => {
+    if (theme === '') {
+      return setTheme('material');
+    } else {
+      return setTheme('');
+    }
+  };
+
+  const downloadTxtFile = () => {
+    if (html === '') {
+      return alert('No Code To Download');
+    }
+    const element = document.createElement('a');
+    const file = new Blob([html], {
+      type: 'text/plain',
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = 'download.txt';
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header'>Code Editor</div>
+      <div className='button-wrapper'>
+        <button className='button-style' onClick={handleCodeRun}>
+          Run <i className='fas fa-angle-double-right'></i>
+        </button>
+        <button className='button-style' onClick={handleTheme}>
+          <i className='fas fa-star-half-alt'></i>
+        </button>
+        <button className='button-style' onClick={downloadTxtFile}>
+          Download Code <i className='fa fa-download' aria-hidden='true'></i>
+        </button>
+      </div>
+      <div className='editor-wrapper'>
+        <div className='output-div'>
+          <Editor
+            language='javascript'
+            theme={theme}
+            value={html}
+            onChange={setHtml}
+          />
+        </div>
+        <div className='output-div'>
+          <iframe
+            srcDoc={srcDoc}
+            title='output'
+            sandbox='allow-scripts'
+            frameBorder='0'
+            width='100%'
+            height='100%'
+          />
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
